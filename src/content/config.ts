fcc8cfc877348a60a70f86f2a626387ee4811217
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2023-02-09 11:06:15 -0800

    chore: Upgrade Astro

---
+++ src/content/config.ts
@@ -0,0 +1,61 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        teaser: z.string().optional(),
+        date: z
+            .string()
+            .or(z.date())
+            .optional()
+            .transform((str) => str ? new Date(str + " 00:00:00") : undefined),
+        tags: z.array(z.string()).optional(),
+        categories: z.array(z.string()).optional(),
+        permalink: z.string().optional(),
+        published: z.boolean().optional(),
+        draft: z.boolean().optional(),
+        // Common Astro schemas
+        pubDate: z
+            .string()
+            .or(z.date())
+            .optional()
+            .transform((val) => val ? new Date(val) : undefined),
+        updatedDate: z
+            .string()
+            .optional()
+            .transform((str) => (str ? new Date(str) : undefined)),
+        heroImage: z.string().optional(),
+    }),
+});
+
+const snippets = defineCollection({
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        teaser: z.string().optional(),
+        date: z
+            .string()
+            .or(z.date())
+            .optional()
+            .transform((str) => str ? new Date(str + " 00:00:00") : undefined),
+        tags: z.array(z.string()).optional(),
+        categories: z.array(z.string()).optional(),
+        permalink: z.string().optional(),
+        published: z.boolean().optional(),
+        draft: z.boolean().optional(),
+        // Common Astro schemas
+        pubDate: z
+            .string()
+            .or(z.date())
+            .optional()
+            .transform((val) => val ? new Date(val) : undefined),
+        updatedDate: z
+            .string()
+            .optional()
+            .transform((str) => (str ? new Date(str) : undefined)),
+        heroImage: z.string().optional(),
+    }),
+});
+
+export const collections = { blog, snippets };
--- src/lib/util.js
+++ src/lib/util.js
@@ -11,116 +11,36 @@
 import path from "path";
 import site from "../data/config.yml";
 import commentThreads from "../data/comments.json";
+import { getCollection } from 'astro:content';
+import { formatJekyllPost } from "@humanwhocodes/astro-jekyll";
 
-//-----------------------------------------------------------------------------
-// Helpers
-//-----------------------------------------------------------------------------
-
-function getDateParts(text) {
-    const dateMatches = /^(\d{4})\-(\d{2})\-(\d{2})/.exec(text);
-    if (dateMatches) {
-        return {
-            year: dateMatches[1],
-            month: dateMatches[2],
-            day: dateMatches[3],
-            toString() {
-                return `${this.year}-${this.month}-${this.day}`
-            }
-        };
-    }
-
-    return undefined;
-
-}
-
-function formatJekyllPosts(posts, type) {
-    return posts.map(post => {
-        const filename = path.basename(post.file, ".md");
-        let slug = filename;
-        let dateParts;
-
-        // is there a date in the filename?
-        dateParts = getDateParts(filename);
-        if (dateParts) {
-            slug = filename.slice(11);
-        }
-
-        // date in the frontmatter overrides the filename
-        if (post.frontmatter.date) {
-            if (typeof post.frontmatter.date === "string") {
-                post.frontmatter.date = new Date(post.frontmatter.date);
-            }
-            dateParts = getDateParts(post.frontmatter.date.toISOString());
-        }
-
-        let url;
-        let urlPath;
-
-        if (post.frontmatter.permalink) {
-
-            url = post.frontmatter.permalink;
-            
-            // format: [ '', 'blog', '2009', '05', '05', 'http-cookies-explained', '' ]
-            const pathParts = post.frontmatter.permalink.split("/");
-            pathParts.shift();  // remove first empty space
-            pathParts.shift();  // remove "blog"
-            pathParts.pop();    // remove last empty space
-            urlPath = pathParts.join("/");
-        } else {
-            url = `/${type}/${dateParts.year}/${dateParts.month}/${slug}/`;
-            urlPath = `${dateParts.year}/${dateParts.month}/${slug}`;
-        }
-
-        const newPost = Object.create(post, {
-            url: { value: url }
-        });
-
-        newPost.urlPath = urlPath;
-
-        if (!newPost.frontmatter.date) {
-            newPost.frontmatter.date = new Date(dateParts);
-        }
-        
-        if (!newPost.frontmatter.pubDate) {
-            newPost.frontmatter.pubDate = newPost.frontmatter.date;
-        }
-
-        if (newPost.frontmatter.updated) {
-            newPost.frontmatter.updated = new Date(newPost.frontmatter.updated);
-        }
-
-        // check for comments
-        const commentThread = commentThreads[new URL(url, site.url).href]
-        newPost.comments = commentThread ? commentThread.comments : null;
-
-        return newPost;
-    }).reverse();
-}
 
 //-----------------------------------------------------------------------------
 // Exports
 //-----------------------------------------------------------------------------
 
 export async function loadBlogPosts() {
+    const posts = (await getCollection('blog'))
+        .map(formatJekyllPost())
+        .reverse()
+        .filter(post => !post.data.draft && (post.data.date - new Date()));
 
-    const postFiles = await import.meta.glob("../pages/_posts/**/*.md");
-    const posts = await Promise.all(Object.values(postFiles).map(async (getInfo) => {
-        const meta = await getInfo();
-        return meta;
-    }));
+    // check for comments
+    posts.forEach(post => {
+        const url = `/${post.collection}/${post.slug})`;
+        const commentThread = commentThreads[new URL(url, site.url).href]
+        post.comments = commentThread ? commentThread.comments : null;
+    });
 
-    return formatJekyllPosts(posts, "blog").filter(post => !post.frontmatter.draft);
+    return posts;
 }
 
 export async function loadSnippets() {
 
-    const postFiles = await import.meta.glob("../pages/_snippets/**/*.md");
-    const posts = await Promise.all(Object.values(postFiles).map(async (getInfo) => {
-        const meta = await getInfo();
-        return meta;
-    }));
-
-    return formatJekyllPosts(posts, "snippets");
+    return (await getCollection('snippets'))
+        .map(formatJekyllPost())
+        .reverse()
+        .filter(post => !post.data.draft && (post.data.date - new Date()))
 }
 
 export async function loadAllContent() {
